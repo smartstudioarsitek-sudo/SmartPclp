70bfececce528eecca3a9a56a5a05a03f34d6ee1
smartstudioarsitek-sudo <smartstudioarsitek@gmail.com>
2026-01-09 07:56:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-# --- NAVIGASI UTAMA ---
-# main.py bertugas sebagai "Launcher" atau Pintu Masuk
+# --- KONFIGURASI UTAMA ---
 st.set_page_config(page_title="Smart Studio Arsitek", layout="wide", page_icon="🏗️")
 
+# --- NAVIGASI ---
+# Pastikan file app_pclp.py dan app_pclp_hidro.py ada di folder yang sama
 pg = st.navigation([
     st.Page("app_pclp.py", title="Desain Sipil (PCLP)", icon="🚜"),
     st.Page("app_pclp_hidro.py", title="Analisis Hidrologi", icon="💧"),
